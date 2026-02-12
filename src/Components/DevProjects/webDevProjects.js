75b469eeb4f5b7f6076418fa02db59650ba2f2b6
Stanley Amunze <stanleyamunze03@gmail.com>
2026-02-12 08:58:09 +0300

    Renamed import statements in webDevProjects.js

--- src/Components/DevProjects/webDevProjects.js
+++ src/Components/DevProjects/webDevProjects.js
@@ -1,8 +1,8 @@
 import NeuroCare from "../../assets/NeuroCare.png";
 import Connectify from "../../assets/Connectify.png";
 import GamerHub from "../../assets/GamerHub.png";
-import SmartWraps from "../../assets/SmartWraps.png";
 import Mune from "../../assets/Mune.png";
+import SmartWraps from "../../assets/SmartWraps.png";
 
 export const devProjects = [
   {
